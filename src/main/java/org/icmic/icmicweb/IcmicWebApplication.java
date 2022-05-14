c0a75ddc8828b564bd3f0f4dda14b03d1dad252e
lichee55 <lichee55@gmail.com>
2022-05-14 20:26:52 +0900

    initial commit

---
+++ src/main/java/org/icmic/icmicweb/IcmicWebApplication.java
@@ -0,0 +1,13 @@
+package org.icmic.icmicweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcmicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcmicWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/icmic/icmicweb/IcmicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.icmic.icmicweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcmicWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
