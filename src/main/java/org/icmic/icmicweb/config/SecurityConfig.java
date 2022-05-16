7c884a1f4ca52850c3c8e2efade3dafc454b363f
lichee55 <lichee55@gmail.com>
2022-05-16 18:19:54 +0900

    [fix] security config

---
+++ src/main/java/org/icmic/icmicweb/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.icmic.icmicweb.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+}
