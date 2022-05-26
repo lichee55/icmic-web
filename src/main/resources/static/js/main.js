9e2a96e7855c9b2b805d11165a3adbc72ffaaf74
gh1224 <rlarkgus0@naver.com>
2022-05-26 18:18:11 +0900

    [add] 슬라이드 점 버튼

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,14 +1,32 @@
 $(function () {
     const btns = $('#bracket li');
+    const circleBtns = $('#slideBtns > div');
     const boxes = $('#linkBoxes > div');
     let currentIdx = 0;
+
+    selectSlide = function (i) {
+        $(btns[currentIdx]).removeClass('selected');
+        $(btns[i]).addClass('selected');
+
+        $(circleBtns[currentIdx]).removeClass('selected');
+        $(circleBtns[i]).addClass('selected');
+
+        $(boxes).css('margin-left', (-645 * i) + 'px');
+        currentIdx = i;
+    };
+
     btns.click(function () {
         for (let i = 0; i < 3; i++) {
             if ($(this).is(btns[i])) {
-                $(btns[currentIdx]).removeClass('selected');
-                $(this).addClass('selected');
-                $(boxes).css('margin-left', (-645 * i) + 'px');
-                currentIdx = i;
+                selectSlide(i);
+            }
+        }
+    });
+
+    circleBtns.click(function () {
+        for (let i = 0; i < 3; i++) {
+            if ($(this).is(circleBtns[i])) {
+                selectSlide(i);
             }
         }
     });
