08c48807d0b03e0a3117fe6a0d3374f885a68f71
gh1224 <rlarkgus0@naver.com>
2022-05-26 12:39:57 +0900

    [add] 링크 박스 슬라이드

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,15 +1,13 @@
 $(function () {
-    const btns = $('.bracket li');
-    const boxes = $('.linkBoxes ul');
+    const btns = $('#bracket li');
+    const boxes = $('#linkBoxes > div');
     let currentIdx = 0;
     btns.click(function () {
         for (let i = 0; i < 3; i++) {
             if ($(this).is(btns[i])) {
                 $(btns[currentIdx]).removeClass('selected');
-                $(boxes[currentIdx]).removeClass('selected');
-
                 $(this).addClass('selected');
-                $(boxes[i]).addClass('selected');
+                $(boxes).css('margin-left', (-645 * i) + 'px');
                 currentIdx = i;
             }
         }
