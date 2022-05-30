c84fa156c3ac47bcd7b91b6652dfa39e5546a0bd
lichee55 <lichee55@gmail.com>
2022-05-30 13:54:32 +0900

    [chore] model attribute

--- src/main/java/org/icmic/icmicweb/controller/BoardController.java
+++ src/main/java/org/icmic/icmicweb/controller/BoardController.java
@@ -8,9 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,28 +17,26 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/board")
-    @ResponseBody
     public String board(Model model, Pageable pageable) {
         Page<BoardDetailDTO> boardList = boardService.findBoardList(pageable);
         model.addAttribute("boardList", boardList);
-        return boardList.toString();
+        return "boardList";
     }
 
     @GetMapping("/board/detail")
-    public String boardDetail(Model model, Long id) {
+    public String boardDetail(@RequestParam Long id, Model model) {
         BoardDetailDTO boardDetailDTO = boardService.findBoardDetail(id);
         model.addAttribute("boardDetail", boardDetailDTO);
         return "boardDetail";
     }
 
     @GetMapping("/board/write")
-    @ResponseBody
     public String boardWrite() {
         return "boardWrite";
     }
 
     @PostMapping("/board/write")
-    public String writeBoard(BoardWriteDTO boardWriteDTO) {
+    public String writeBoard(@ModelAttribute BoardWriteDTO boardWriteDTO) {
         Long newBoardId = boardService.writeBoard(boardWriteDTO);
         return "redirect:/board/detail?boardId=" + newBoardId;
     }
